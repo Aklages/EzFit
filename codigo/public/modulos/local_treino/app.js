e04f7e1799a5e97f74509a44e76f133520d71d6f
André Lages <ak1lages@gmail.com>
2025-05-16 18:26:18 -0300

    get locals from proximity

--- codigo/public/modulos/local_treino/app.js
+++ codigo/public/modulos/local_treino/app.js
@@ -38,14 +38,63 @@ function getLocalizacao(){
 }
 
 function getmap(){
-    Street = $("#formStreet").val();
-    Postcode = $("#formPostcode").val();
-    Place = $("#formPlace").val();
-    Region = $("#formRegion").val();
+    const Street = $("#formStreet").val();
+    const Postcode = $("#formPostcode").val();
+    const Place = $("#formPlace").val();
+    const Region = $("#formRegion").val();
+    const query = `${Street}, ${Postcode}, ${Place}, ${Region}`;
 
-    fetch(`https://api.mapbox.com/search/geocode/v6/forward?q=${encodeURIComponent(Street, Postcode, Place, Region)}&access_token=${key}`).
-    then(res => res.json())
+    let ponto_norte, ponto_leste, ponto_sul, ponto_oeste;
+
+    fetch(`https://api.mapbox.com/search/geocode/v6/forward?q=${encodeURIComponent(query)}&access_token=${key}`)
+    .then(res => res.json())
     .then(data => {
+        longitude = Number(data.features[0].geometry.coordinates[0]);
+        latitude = Number(data.features[0].geometry.coordinates[1]);
 
+        let interesse = $("#formInteresse").val();
+        let distancia = $("#formDistancia").val();
+
+        switch(distancia){
+            case "1":
+                ponto_norte = latitude + 0.008983;
+                ponto_leste = longitude + 0.009756;
+                ponto_sul = latitude - 0.008983;
+                ponto_oeste = longitude - 0.009756;
+                break;
+            case "3":
+                ponto_norte = latitude + 0.026949;
+                ponto_leste = longitude + 0.029268;
+                ponto_sul = latitude - 0.026949;
+                ponto_oeste = longitude - 0.029268;
+                break;
+            case "5":
+                ponto_norte = latitude + 0.044915;
+                ponto_leste = longitude + 0.048780;
+                ponto_sul = latitude - 0.044915;
+                ponto_oeste = longitude - 0.048780;
+                break;
+            case "0":
+                ponto_norte = 100;
+                ponto_leste = 100;
+                ponto_sul = -100;
+                ponto_oeste = -100;
+        }
+
+        fetch(`/locais/?tipo=${interesse}`)
+        .then(res => res.json())
+        .then(local => {
+            local.forEach(element => {
+                let lat = Number(element.coordenadas.latitude);
+                let long = Number(element.coordenadas.longitude);
+
+                if(lat <= ponto_norte &&
+                    long <= ponto_leste &&
+                    lat >= ponto_sul &&
+                    long >= ponto_oeste){
+                    console.log(element);
+                }
+            });
+        })
     })
 }
