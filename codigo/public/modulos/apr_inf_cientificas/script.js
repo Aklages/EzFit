b5c13b09ffa72df087651da0d3805d76a66f9b14
Diego-ia-tl <limavolponidiego@gmail.com>
2025-05-16 17:55:37 +0000

    Apresentar informações cientificas.

---
+++ codigo/public/modulos/apr_inf_cientificas/script.js
@@ -0,0 +1,26 @@
+// URL do JSON Server
+const URL = 'http://localhost:3000/conteudo/';
+const container = document.getElementById('card-container');
+// Buscar dados do JSON Server e gerar cards
+fetch(URL)
+.then(response => response.json())
+.then(data => {
+data.forEach(conteudo=> {
+const card = document.createElement('div');
+card.classList.add('card');
+
+card.innerHTML = `
+<img src="${conteudo.imagem}" alt="${conteudo.titulo}">
+<div class="card-content">
+<h2>${conteudo.titulo}</h2>
+<p>${conteudo.descricao}</p>
+</div>
+`;
+
+container.appendChild(card);
+});
+})
+.catch(error => {
+console.error('Erro ao carregar os conteudo:', error);
+container.innerHTML = "<p>Erro ao carregar conteudo. Verifique o servidor.</p>";
+});
